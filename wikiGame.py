db80b0de781fb83b34813d813ac59cf345a10aa8
Vukasin_Vasiljevic_581_2016 <vukasin.vasiljevic@gmail.com>
2020-08-28 09:41:16 +0200

    updating wikiSearch function

--- wikiGame.py
+++ wikiGame.py
@@ -12,28 +12,20 @@ Created on Tue Jun 23 14:58:48 2020
      Output: Path from start article to goal article.
      
      Example:
-         Enter start article: Cat
+         Enter start article: Дрводеља
 
-         Enter goal article: Dog
+         Enter goal article: Емили Мортимер
          Begining search
          
          Goal article found!
          
-         ['Cat', 'Domestication', 'Cat_anatomy', 'Near_East', 'en.wikipedia.org', 
-         'Old_English', 'en.wiktionary.org', 'Scientific_name', 
-         'International_Commission_on_Zoological_Nomenclature', 'Family_(biology)', 
-         'Taming', 'Preadaptation', 'Cat_breeds', 'European_wildcat', 'Cervical_vertebrae',
-         'Eye_socket', 'Digitigrade', 'Dewclaw']
+         Full path from start article to goal article is ['Дрводеља', '2002', '1. децембар']
          
-         Diameter of graph is 2
-         
-         Eccentricity of graph is {'Cat': 1, 'Domestication': 2, 'Cat_anatomy': 2, 'Near_East': 2,
-         'Old_English': 2, 'Scientific_name': 2, 'International_Commission_on_Zoological_Nomenclature': 2, 
-         'Family_(biology)': 2, 'Taming': 2, 'Preadaptation': 2, 'Cat_breeds': 2, 'European_wildcat': 2, 
-         'Cervical_vertebrae': 2, 'Eye_socket': 2, 'Digitigrade': 2, 'Dewclaw': 2}
+
          
          
-    Note: This algorithm does not provide the shortest path, it is based on uninformed search
+         
+    Note: This algorithm provides the shortest path, it is based on uninformed search
           algorithm (Breadth First Search). Also, this is not optimal nor complete algorithm i.e. 
           this algorithm doesn't always find path
 
@@ -48,20 +40,41 @@ import json
 import wikipedia
 from time import sleep
 from urllib3.exceptions import InsecureRequestWarning
+from collections import deque
+
 
 # Suppress only the single warning from urllib3 needed.
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
 # set language on wikipedia
 wikipedia.set_lang('sr')
 # Entering start and goal article 
 startArticle = str(input('Enter start article: '))
 goalArticle = str(input('Enter goal article: '))
-startArticle = startArticle.replace(" ", "_")
-goalArticle = goalArticle.replace(" ", "_")
+
 visited = [startArticle]
 hyperlinks={}
 path=[startArticle]
 level=0
+
+
+# Return depth of nested dictionary
+def depth(d):
+    queue = deque([(id(d), d, 1)])
+    memo = set()
+    while queue:
+        id_, o, level = queue.popleft()
+        if id_ in memo:
+            continue
+        memo.add(id_)
+        if isinstance(o, dict):
+            queue += ((id(v), v, level + 1) for v in o.values())
+    return level
+
+def read_json_file(filename):
+    with open(filename) as f:
+        js_graph = json.load(f)
+    return nx.node_link_graph(js_graph)
 
 
 # Function that gets all hyperlinks from article page
@@ -125,19 +138,31 @@ def deeperLevel(dictionary, level=0):
         elif level==1:
             for parent_tree in new_tree.values():
                 for parent in parent_tree:
-                    #wikiSearchBfs(parent_tree, parent, recursion=1)
-                    print(parent)
-                    print(parent_tree)
-                    break
-                break
-            
-def thirdLevel(dictionary):
-    for new_tree in dictionary.values():
-        for parent_tree in new_tree.values():
-            for parent in parent_tree.values():
-                for kid in parent:
-                        wikiSearchBfs(parent, kid, recursion=1)
-    
+                    path.append(parent)
+                    wikiSearchBfs(parent_tree, parent, recursion=1)
+                    path.pop()
+
+
+# Update wikisearch
+def wikiSearch(nested_dict):
+        k=""
+        for k,v in nested_dict.items():
+                k=k
+                for child in v:
+                    if child!=None:
+                        #print(child)
+                        if child==goalArticle:
+                            print("Found!")
+                        else:
+                            if child not in visited:
+                                visited.append(child)
+                                children = getLinksFrom(child)
+                                if children!=None:
+                                    nested_dict[k][child] = getLinksFrom(child)
+                            else:
+                                pass
+        #print(nested_dict)
+        wikiSearch(nested_dict[k])
 
 # wikiSearchBfs function is based on Breacth first Search algorithm 
 def wikiSearchBfs(tree_dict1, parent, recursion=0, level=0):
@@ -153,12 +178,13 @@ def wikiSearchBfs(tree_dict1, parent, recursion=0, level=0):
                         print('Goal article found!\n')
                         print("Full path from start article to goal article is {}".format(path)) # Print path to goal article
                         print('\n')
-            
+                        print("Depth of tree is: {}".format(depth(tree_dict)))
                         tree_dict1[parent][i] = children # Append last article into visited nodes
                         
-                        G = toGraph(tree_dict) # Convert nested dictionary to graph
-                        print("Diameter of a graph is {}".format(nx.diameter(G)))
-                        #print("\nEccentricity of graph is {}".format(nx.eccentricity(G)))                  
+                        #G = toGraph(tree_dict) # Convert nested dictionary to graph
+                        #print("Diameter of a graph is {}".format(nx.diameter(G)))
+                        #print("\nEccentricity of graph is {}".format(nx.eccentricity(G))) 
+                        
                         # load nested ditionaty into json file 
                         with open('treeData.json', 'w') as outfile:
                             json.dump(convert(tree_dict), outfile)
@@ -170,11 +196,8 @@ def wikiSearchBfs(tree_dict1, parent, recursion=0, level=0):
                         if k in visited:
                             del children[k]  # if so, delete that node from children list
                     tree_dict1[parent][i] = children
-                
-                    
-              
-        if recursion==0:
-            
+        
+        if recursion==0:        
             try:
                 # Second degree
                 print("Second Degree!")
